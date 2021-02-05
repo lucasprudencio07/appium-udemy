bde9b630e407eeb57d0f26afdc6c6ce55de8d352
lucasprudencio07 <lucas.santos1@wexinc.com>
2021-02-05 16:44:34 -0300

    Back button to go back to the app

--- src/test/java/application_ecommerce/ECommerceTests.java
+++ src/test/java/application_ecommerce/ECommerceTests.java
@@ -3,6 +3,8 @@ package application_ecommerce;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -200,8 +202,9 @@ public class ECommerceTests extends  BaseEcommerceTests {
 
         androidDriver.findElement(By.name("q")).sendKeys("Techspot");
         androidDriver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+        androidDriver.navigate().back();
 
-        Thread.sleep(7000);
+        Thread.sleep(4000);
 
         /*androidDriver.runAppInBackground(Duration.ofSeconds(2));
 
